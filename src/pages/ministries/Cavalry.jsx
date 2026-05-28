30fd3affbf3c996ba2a478771242e0e8b4fe3bd0
Rhaz <83931688+Rhaz05@users.noreply.github.com>
2026-05-28 08:28:44 +0800

    Mayunis4 (#9)
    * ui changes
    
    * churchCovenant, Missions
    
    ---------
    
    Co-authored-by: shironeek0 <shironeek@gmail.com>

--- src/pages/ministries/Cavalry.jsx
+++ src/pages/ministries/Cavalry.jsx
@@ -3,11 +3,13 @@ import React from 'react'
 function Cavalry() {
   return (
     <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci fugit animi aliquid iusto
-      eius sapiente dignissimos quaerat inventore necessitatibus accusamus error porro dolorum
-      doloremque provident et impedit ducimus tempora modi asperiores repellat reprehenderit, non
-      officiis delectus. Laboriosam obcaecati eum aperiam commodi magnam amet. Neque beatae soluta
-      repellendus eum quisquam repellat!
+      With hearts committed to serving God and reaching souls, the members 
+      of Grace Baptist Church faithfully minister to the soldiers of the 2nd 
+      Cavalry Squadron every 1st and 3rd of the month. Through Bible studies, 
+      fellowship, and the preaching of God's Word, we aim to strengthen lives, 
+      encourage hearts, and lead brave men in uniform closer to Christ. As they 
+      serve and protect our nation, we are honored to share with them the hope, 
+      peace, and salvation found in the Lord Jesus Christ.
     </div>
   )
 }
